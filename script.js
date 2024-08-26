fb269b41b413107105e4e08fd08e965aca26f5f9
Tharun theja Boyalla <tharunthejab@cabcsgroup-india.com>
2024-08-26 11:16:00 +0530

    Responsive, Hamburger menu Completed

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const mainHome = document.querySelector(".intro-text");
 const mainText = document.querySelector(".intro-text h1");
 const gameLink = document.querySelector(".game-link");
+const hamBurgerMenu = document.querySelector(".hamburger-menu");
 const startButton = document.getElementById("start");
+const hamBurger = document.getElementById("hamburger-icon");
 
 function startFunction (){
-    mainHome.style.top = "-198px";
+    mainHome.style.top = "-600px";
     mainText.style.fontSize = "24px";
+    startButton.style.bottom = "-800px"
     setTimeout(()=>{
         mainHome.style.display = "none";
-        gameLink.style.visibility = "visible"
+        gameLink.style.opacity = 1;
     },700)
 }
+
+function hamburger(){
+    const currentRight = window.getComputedStyle(hamBurgerMenu).right;
+    if (currentRight === '0px') {
+        hamBurgerMenu.style.right = "-500px"; 
+    } else {
+        hamBurgerMenu.style.right = "0"; 
+    }
+}
+
+hamBurger.addEventListener('click',hamburger)
