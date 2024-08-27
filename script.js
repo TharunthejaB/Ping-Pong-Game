d147200fb567c0c2f33ea32f93d6957ce605613c
Tharun theja Boyalla <tharunthejab@cabcsgroup-india.com>
2024-08-27 11:27:53 +0530

    Home page 30%

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-const mainHome = document.querySelector(".intro-text");
+const mainHome = document.querySelector(".main-home");
+const mainTextBox = document.querySelector(".intro-text");
 const mainText = document.querySelector(".intro-text h1");
 const gameLink = document.querySelector(".game-link");
 const hamBurgerMenu = document.querySelector(".hamburger-menu");
 const startButton = document.getElementById("start");
 const hamBurger = document.getElementById("hamburger-icon");
 const menu = document.getElementById('hamburger-icon');
-const lines = document.querySelectorAll('.not-active')
+const lines = document.querySelectorAll('.not-active');
+const gameArea = document.querySelector(".game-area");
 
 
 function startFunction (){
-    mainHome.style.top = "-600px";
+    mainTextBox.style.top = "-600px";
     mainText.style.fontSize = "24px";
     startButton.style.bottom = "-800px"
     setTimeout(()=>{
         mainHome.style.display = "none";
         gameLink.style.opacity = 1;
     },700)
+    setTimeout(()=>{
+        gameArea.style.opacity = 1;
+    },900)
+}
+
+function revStartFunction (){
+    gameArea.style.opacity = 0;
+    setTimeout(()=>{
+        mainHome.style.display = "flex";
+        gameLink.style.opacity = 0;
+    },700)
+    setTimeout(()=>{
+        mainTextBox.style.top = "0px";
+        mainText.style.fontSize = "96px";
+        startButton.style.bottom = "0px";
+    },900)
 }
 
 function hamburger(){
